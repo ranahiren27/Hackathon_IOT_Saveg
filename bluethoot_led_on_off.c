a0f555daf2a2a2cf118a4444b359978a580534bf
ranahiren27 <ranahiren27@gmail.com>
2019-03-02 16:42:37 +0530

    bluethoot.c added

---
+++ bluethoot_led_on_off.c
@@ -0,0 +1,6 @@
+user_reg=name email mobile password department 
+question =subject_id questions date //
+answer=subject_id answer date and time //
+feedback =comment_id  email comment date and time //
+quiz user_id password subject//
+faculty f_id department email mobile name password //
